6f3645efcdd2202a3ba9d76676902412039e3f5b
mj <maja.szerszen.7@gmail.com>
2025-01-14 21:37:06 +0100

    fix functions of class UkladSloneczny

--- project.py
+++ project.py
@@ -11,14 +11,17 @@ przechowywania.
 Literatura: T.H. Cormen, C.E. Leiserson, R.L. Rivest, "Wprowadzenie do algorytmów," WNT, Warszawa, 2001.
 """
 
+
+'''
+STRUKTURY DANYCH
+'''
 class CialoNiebieskie():
     def __init__(self, nazwa=None, odleglosc=None, masa=None, okresObiegu=None):
         self.nazwa = nazwa
         self.odleglosc = odleglosc
         self.masa = masa
         self.okresObiegu = okresObiegu
-        self.next = None
-        
+
     def edit(self, nazwa=None, odleglosc=None, masa=None, okresObiegu=None):
         if nazwa is not None:
             self.nazwa = nazwa
@@ -28,85 +31,56 @@ class CialoNiebieskie():
             self.masa = masa
         if okresObiegu is not None:
             self.okresObiegu = okresObiegu
-class List():
+
+class UkladSloneczny():
     def __init__(self):
-        self.head = CialoNiebieskie()
+        self.objects = [] 
+
+    def __getitem__(self, index):
+        if (index < len(self.objects)):    
+            return self.objects[index]
+        print("Układ Słoneczny nie zawiera tylu elementów")
+
+    def __setitem__(self, index, value):
+        if (index < len(self.objects)):    
+            self.objects[index] = value
+        print("Układ Słoneczny nie zawiera tylu elementów")
         
-    def append(self, nazwa, odleglosc, masa, okresObiegu):
-        appended_node = CialoNiebieskie(nazwa, odleglosc, masa, okresObiegu)
-        if self.head.nazwa == None:
-            self.head = appended_node
-        else: 
-            counter = self.head
-            while(counter.next != None):
-                counter=counter.next
-            counter.next = appended_node
-            
+    def add(self, nazwa, odleglosc, masa, okresObiegu):
+        self.objects.append(CialoNiebieskie(nazwa, odleglosc, masa, okresObiegu))
+       
+    def remove_by_name(self, nazwa):
+        self.objects = [obj for obj in self.objects if obj.nazwa != nazwa]
+        
+    def edit_by_name(self, doEdytowania, nazwa=None, odleglosc=None, masa=None, okresObiegu=None):
+        for obj in self.objects:
+            if (obj.nazwa == doEdytowania):
+                obj.edit(nazwa, odleglosc, masa, okresObiegu)
+                
+    def edit_by_position(self, doEdytowania, nazwa=None, odleglosc=None, masa=None, okresObiegu=None):
+        if doEdytowania < len(self.objects):
+            self[doEdytowania].edit(nazwa, odleglosc, masa, okresObiegu)
+        else:
+            print("Układ Słoneczny nie zawiera tylu elementów")
+                
     def display(self):
-        counter=self.head
-        print(counter.nazwa)
-        while counter.next!=None:
-              print(f"Nazwa: {counter.nazwa}, Odległość: {counter.odleglosc}, Masa: {counter.masa}, Okres obiegu: {counter.okresObiegu}")
-              counter = counter.next
-              
-    def length(self):
-        counter = self.head
-        if self.head.nazwa==None:
-            return 0
-        output = 1
-        while counter.next != None:
-            counter=counter.next
-            output +=1
-        return output
-    
-    def remove(self, node): # zmieniamy na removeByName albo removeByPosition?
-        counter=self.head
-        if counter == node:
-            self.head = counter.next
-            return
-        while counter.next!=None and counter.next != node:
-            counter = counter.next
-        if (counter.next == None):
-            print("Brak ciała niebieskiego w układzie słonecznym")
-            return
-        tmp = counter.next
-        counter.next = tmp.next
-        '''
-        EDYTOWANIE - jak ma wygladac? czy mozna lista.edit(planeta, ...wszystkie dane..)
-        czy np bierze liczbe - pozycje na liscie i tylko ją edytuje?
-        '''
-    def editByName(self, nazwaObiektu, nazwa=None, odleglosc=None, masa=None, okresObiegu=None):
-        counter = self.head
-        while counter is not None:  
-            if counter.nazwa == nazwaObiektu:  
-                counter.edit(nazwa, odleglosc, masa, okresObiegu)
-                return
-            counter = counter.next
-        print("Brak ciała niebieskiego w układzie słonecznym")
+        print("Obiekty w Układzie Słonecznym:")
+        for obj in self.objects:
+            print(f"Nazwa: {obj.nazwa}, Odległość: {obj.odleglosc}, Masa: {obj.masa}, Okres obiegu: {obj.okresObiegu}")
         
-    def editByPosition(self, pozycja, nazwa=None, odleglosc=None, masa=None, okresObiegu=None):
-        i = 0
-        counter = self.head
-        while counter is not None:  
-            if i == pozycja:  
-                counter.edit(nazwa, odleglosc, masa, okresObiegu)
-                return
-            counter = counter.next
-            i+=1
-        print("Brak ciała niebieskiego w układzie słonecznym")
+'''
+ALGORYTMY SORTOWANIA - BUCKET SORT
+'''
 
 
-lista = List()
-lista.append("Słońce", 0,0.2, 0)
-lista.append("Ziemia", 11, 5, 365)
+
+lista = UkladSloneczny()
+lista.add("Słońce", 0,0.2, 0)
+lista.add("Ziemia", 11, 5, 365)
 lista.display()
-counter = lista.head
-i = 0
-while counter != None:
-    if i == 1:
-        counter.edit(odleglosc = 23)
-    i+=1
-    counter=counter.next
-    
-        
+
+lista.edit_by_position(1,masa=123)  
+lista.display()
+#bucket_sort_descending(lista.objects, dana = lambda obj: obj.masa)
+
 lista.display()
